d76c7d2a5c0958a3cb3c5e61418d95f45342799f
xander <xander.king@ents24.com>
2020-12-12 22:03:48 +0000

    Basic API call & analysis

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
 import { createStore } from 'vuex'
 import staticGraunPersonnel from '../../data/the-guardian-personnel.json'
+import staticArticles from '../../data/articles.test.json'
+
 import ky from 'ky';
 
 const apiKey = '9db526cb-6cfe-45a3-b45f-c89483d43628'
+const apiUrl =new URL('https://content.guardianapis.com/search')
+apiUrl.searchParams.append('api-key',apiKey)
 
 export default createStore({
   modules: {
@@ -13,17 +17,93 @@ export default createStore({
         personnel: staticGraunPersonnel
       }, 
       getters: {
-        personnel () {  },
+        // Curried syntax to allow parameter
+        authorFromArticle: (state) => (article) => {
+          if(Array.isArray(article.tags) === false) {
+            console.log('no tags')
+            return null
+          }
+
+          const contributorTag = article.tags.find(tag => tag.type === 'contributor')
+
+          if (!contributorTag) {
+            console.log('no contributor')
+            return null
+          }
+
+          if(!contributorTag.webTitle) {
+            console.log('no webTitle')
+            return null
+          }
+
+          const author = state.personnel.find(person => person.name === contributorTag.webTitle)
+
+          if(!author) {
+            console.log(contributorTag.webTitle,'no matching author')
+            return null
+          }
+
+          console.log(author.name, '✓')
+
+          return author
+        },
+
         articles(state){
           console.log('getting')
           return state.articles
-        }
+        },
+
+        statsOverall(state, getters){
+
+          const stats = {
+            foundTally:0,
+            oxTally: 0,
+            plebTally: 0,
+            unknownTally:0,
+            total: getters.articles.length,
+          }          
+
+          getters.articles.forEach( article => {
+            let author = getters.authorFromArticle(article)
+
+            if(author){
+              stats.foundTally++
+            }
+
+            if (!author || author.oxbridge === null) {
+              stats.unknownTally++
+            } else if(author.oxbridge){
+              stats.oxTally++
+            } else {
+              stats.plebTally++
+            }
+
+          })
+
+          return stats
+          
+        },
       },
       actions: {
-        async loadArticles () { 
-            const parsed = await ky.get('https://content.guardianapis.com/search?q=opinion&from-date=2020-12-10&api-key=test').json();
-            console.log('GOT response',parsed)
-            this.commit('graun/articles', parsed.response.results)
+        async loadArticles () {
+
+          apiUrl.searchParams.append("from-date", "2020-12-12")
+          apiUrl.searchParams.append("to-date", "2020-12-12")
+          apiUrl.searchParams.append("show-tags", "contributor")
+          apiUrl.searchParams.append("section", "commentisfree")
+
+          // const parsed = await ky.get(`https://content.guardianapis.com/search?q=opinion&from-date=2020-12-10&show-tags=contributor&api-key=${apiKey}`).json();
+          
+          const parsed = await ky.get(apiUrl).json();
+            
+          console.log('GOT response',parsed)
+          this.commit('graun/articles', parsed.response.results)
+
+            // setTimeout(() => {
+            //   this.commit('graun/articles', staticArticles.response.results)
+            // }, 1000)
+
+            
         } 
       },
       mutations: {
--- src/views/Organ.vue
+++ src/views/Organ.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="publisher">
     <h2>Today's Opinion Articles ({{graunArticles.length}})</h2>
-    <p v-for="(article, index) in graunArticles" :key="index">{{article.webTitle}}</p>
-    <h2>Personnel ({{graunPersonnel.length}})</h2>
+    <div>Total {{statsOverall.total}}</div>
+    <div>Found {{statsOverall.foundTally}}</div>
+    <div>Oxbridge {{statsOverall.oxTally}}</div>
+    <div>Pleb {{statsOverall.plebTally}}</div>
+    <div>Pass {{statsOverall.unknownTally}}</div>
+    <p v-for="(article, index) in graunArticles" :key="index">
+      {{article.sectionName}} - {{article.tags[0].webTitle}}
+      <a :href=article.webUrl>{{article.webTitle.substring(0,25)}}</a>
+    </p>
+    <!-- <h2>Personnel ({{graunPersonnel.length}})</h2>
     <p v-for="(person, index) in graunPersonnel" :key="index">
       {{person.name}} - {{[person['uni-text'], person['college-text']].join(', ')}}
-    </p>
+    </p> -->
   </div>
 </template>
 
@@ -13,18 +21,19 @@
 export default {
   mounted(){
     this.$store.dispatch('graun/loadArticles')
+    
   },
-  data(){
-    return {
-      name : 'moose'
-    }
-  },
+
   computed : {
+    statsOverall(){
+      return this.$store.getters['graun/statsOverall']
+    },
     graunPersonnel(){
       return this.$store.state.graun.personnel
     },
     graunArticles(){
       return this.$store.getters['graun/articles']
+     
     }
   }
 }
